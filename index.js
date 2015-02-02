b908714cd0dd0bb537e2f03a6e4a47dc84e8b9c2
Max Gfeller <max.gfeller@gmail.com>
2015-02-02 22:01:21 +0100

    Implement logic

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-module.exports = function(giffer, limit) {
-  // First, check number of already downloaded gifs
-  console.log(giffer.seqDb)
+var through = require('through2')
+var fs = require('fs')
+
+var n = 0
+function prepare(giffer, limit) {
+  var toDelete = []
+
+  giffer.seqDb.createReadStream({
+    reverse: true,
+    gt: 1
+  }).pipe(through(function(k, v) {
+    if (n++ > limit) toDelete.push(v)
+  }))
+
+  toDelete.forEach(function(k) {
+  })
+}
+
+function noop() {}
+
+module.exports = function(giffer, opts) {
+  // prepare(giffer, opts.limit)
+  giffer.pre('download', function(next) {
+    if (n++ <= opts.limit) return next()
+
+    giffer.seqDb.createReadStream({
+      limit: 1,
+      gt: 1,
+      reverse: true
+    })
+    .on('data', function(kv) {
+      console.log('got data')
+      giffer.seqDb.del(kv.key, noop)
+      giffer.urlDb.get(kv.value, function(err, o) {
+        if (err) throw new Error('There was an error deleting an old GIF')
+        console.log(o)
+        fs.unlink(giffer.outDir + '/' + o.filename, noop)
+        giffer.urlDb.del(kv.value, noop)
+      })
+    })
+    next()
+  })
 }
