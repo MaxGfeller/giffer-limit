f11f908e754f269531c2e1b915f857af8b28a67a
Max Gfeller <max.gfeller@gmail.com>
2015-02-02 22:03:26 +0100

    Provide a testadapter

--- tests/testadapter.js
+++ tests/testadapter.js
@@ -6,9 +6,12 @@ inherits(TestAdapter, EventEmitter)
 function TestAdapter() {}
 
 TestAdapter.prototype.start = function() {
-    setTimeout(this.emit.bind(this, 'gif', 'http://d3dsacqprgcsqh.cloudfront.net/photo/axNxReD_460sa_v1.gif', { origin: 'test' }), 2000)
-    setTimeout(this.emit.bind(this, 'gif', 'http://d3dsacqprgcsqh.cloudfront.net/photo/ae33vPQ_460sa_v1.gif', { origin: 'test2' }), 2100)
-    setTimeout(this.emit.bind(this, 'gif', 'http://img-9gag-lol.9cache.com/photo/aqZZpqL_460sa_v1.gif', { origin: 'test3' }), 2100)
+    setTimeout(this.emit.bind(this, 'gif', 'http://localhost:9876/1.gif', { origin: 'test' }), 2000)
+    setTimeout(this.emit.bind(this, 'gif', 'http://localhost:9876/2.gif', { origin: 'test2' }), 2100)
+    setTimeout(this.emit.bind(this, 'gif', 'http://localhost:9876/3.gif', { origin: 'test3' }), 2300)
+    setTimeout(this.emit.bind(this, 'gif', 'http://localhost:9876/4.gif', { origin: 'test4' }), 2500)
+    setTimeout(this.emit.bind(this, 'gif', 'http://localhost:9876/5.gif', { origin: 'test5' }), 2700)
+    setTimeout(this.emit.bind(this, 'gif', 'http://localhost:9876/6.gif', { origin: 'test6' }), 2900)
 }
 
 TestAdapter.prototype.stop = function() {}
