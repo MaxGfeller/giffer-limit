6c61ea893817565de53284e961a759e6ec0e1024
Max Gfeller <mg@cyon.ch>
2015-09-01 11:40:01 +0200

    Implement logic

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 var through = require('through2')
 var fs = require('fs')
+var concat = require('concat-stream')
 
 var n = 0
-function prepare(giffer, limit) {
-  var toDelete = []
+function prepare (giffer, limit) {
+  n = 0
+  var ops = []
 
-  giffer.seqDb.createReadStream({
-    reverse: true,
-    gt: 1
-  }).pipe(through(function(k, v) {
-    n++
-    if (n > limit) toDelete.push(v)
-  }))
-
-  toDelete.forEach(function(k) {
-  })
+  giffer.seqDb.createReadStream()
+    .on('data', function (kv) {
+      if (++n <= limit) return
+      ops.push({ type: 'del', key: kv.value })
+    })
+    .on('end', function () {
+      if (ops.length === 0) return
+      n = n - ops.length
+      giffer.urlDb.batch(ops, function (err) {
+        if (err) throw err
+      })
+    })
 }
 
-function noop() {}
-
-module.exports = function(giffer, opts) {
-  // prepare(giffer, opts.limit)
+module.exports = function (giffer, opts) {
+  prepare(giffer, opts.limit)
   giffer.pre('download', function(next) {
-    n++
-    if (n <= opts.limit) return next()
+    if (++n <= opts.limit) return next()
 
     giffer.seqDb.createReadStream({
       limit: 1,
       gt: 1,
-      reverse: true
+      reverse: false
     })
-    .on('data', function(kv) {
-      console.log('got data')
-      giffer.seqDb.del(kv.key, noop)
-      giffer.urlDb.get(kv.value, function(err, o) {
+    .on('data', function (kv) {
+      --n
+      giffer.urlDb.get(kv.value, function (err, o) {
         if (err) throw new Error('There was an error deleting an old GIF')
-        console.log(o)
-        fs.unlink(giffer.outDir + '/' + o.filename, noop)
+        // fs.unlink(giffer.outDir + '/' + o.filename, noop)
         giffer.urlDb.del(kv.value, noop)
       })
     })
     next()
   })
 }
+
+function noop () {}
