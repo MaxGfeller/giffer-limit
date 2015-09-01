2fbab0882f63370f4025cb48b9ea017e519accf7
Max Gfeller <mg@cyon.ch>
2015-09-01 11:41:11 +0200

    Correctly test use cases

--- tests/test-index.js
+++ tests/test-index.js
@@ -4,6 +4,8 @@ var Giffer = require('giffer')
 var TestAdapter = require('./testadapter')
 var spawn = require('child_process').spawn
 var fs = require('fs')
+var rimraf = require('rimraf')
+var concat = require('concat-stream')
 
 // start testserver
 var ls = spawn('node', ['server/server.js'], {
@@ -12,7 +14,8 @@ var ls = spawn('node', ['server/server.js'], {
 
 var db = levelup('whatever', { db: require('memdown'), valueEncoding: 'json' })
 
-test('Test functionality of giffer-limit', function(t) {
+test('Test functionality of giffer-limit', function (t) {
+  setUp()
   var giffer = new Giffer({
     db: db,
     adapters: [ new TestAdapter({}) ],
@@ -21,9 +24,8 @@ test('Test functionality of giffer-limit', function(t) {
   giffer.plugin(require('../'), { limit: 5 })
   giffer.start()
   var i = 0
-  giffer.on('gif', function() {
-    i++
-    if (i < 6) return
+  giffer.on('gif', function () {
+    if (++i < 6) return
 
     var files = fs.readdirSync(giffer.outDir)
     t.equals(files.length, 5, 'one gif should have been deleted')
@@ -32,11 +34,25 @@ test('Test functionality of giffer-limit', function(t) {
   })
 })
 
-// test('Maximum number reached at start', function(t) {
-//   var fullDb = levelup('full', { db: require('memdown') })
-//   var giffer = new Giffer({
-//     db: fullDb,
-//     adapters: [],
-//     outputDir: __dirname + '/tmp'
-//   })
-// })
+test('Maximum number reached at start', function (t) {
+  var giffer = new Giffer({
+    db: db,
+    adapters: [],
+    outputDir: __dirname + '/tmp'
+  })
+  giffer.plugin(require('../'), { limit: 2 })
+  giffer.start()
+  setTimeout(function () {
+    var files = fs.readdirSync(giffer.outDir)
+    t.equals(files.length, 2, 'only two gifs must be left')
+    giffer.createSeqReadStream().pipe(concat(function (data) {
+      t.equals(data.length, 2, 'only two entries left in db')
+      t.end()
+    }))
+  }, 1000)
+})
+
+function setUp () {
+  rimraf.sync(__dirname + '/tmp')
+  fs.mkdirSync(__dirname + '/tmp')
+}
